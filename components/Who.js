57b0e93db414bd6d8132d979b3a62a9ec9932efe
Louai Bouzaher <58881632+louaibouzaher@users.noreply.github.com>
2021-02-21 21:58:11 +0100

    fixed images

--- components/Who.js
+++ components/Who.js
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import { Card } from "./Who/Card";
+import styles from "../styles/Who.module.css";
 
 export const Who = () => {
   const mission = [
@@ -6,25 +8,25 @@ export const Who = () => {
       title: "Awarness",
       text:
         "Increase awareness about CRC and CRC screening. Improve detection through decreasing barriers to participation in CRC screening.",
-      src: "/assets/awarness.svg",
+      src: "/assets/awarness.jpg",
     },
     {
       title: "Advocacy",
       text:
         "Involve doctors and general practitioners in advising patients about the necessity of early screening starting at the age of 45. Advocate with stakeholders to establish an organized CRC screening program in Lebanon while assuring access to high quality screening.",
-      src: "/assets/advocacy.svg",
+      src: "/assets/advocacy.jpg",
     },
     {
       title: "Support",
       text:
         "SAID NGO team members help in directing people to reliable laboratories for FIT and taking appointments to those who ask for help in that matter. SAID NGO survivors share their experience with compassion, knowledge and understanding via support groups to colorectal patients, and caregivers.",
-      src: "/assets/support.svg",
+      src: "/assets/support.jpg",
     },
   ];
   return (
     <div className="w-full bg-blue-900 px-32 py-20 flex flex-col items-center">
-      <img src="/assets/logowhite.svg" alt="" className="h-36" />
-      <p className="w-1/2 text-white py-3 text-center">
+      <img src="/assets/white.svg" alt="" className="h-36" />
+      <p className="w-1/2 text-white py-4 text-center">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
         blanditiis alias repellendus veniam nihil quo minus fuga nostrum quaerat
         impedit, tempora voluptas ut possimus dolorum expedita distinctio iure
@@ -36,9 +38,8 @@ export const Who = () => {
         ))}
       </div>
       <div className="flex flex-row pt-20 justify-center items-center">
-        <div className="bg-white rounded-full h-64 w-64 p-5">
-          <img src="/assets/vision.svg" alt="" className="h-full" />
-        </div>
+        <div className={"rounded-full " + styles.visionBg}></div>
+
         <div className="flex flex-col p-10">
           <h1 className="text-6xl py-4 text-yellow-500">Vision</h1>
           <p className="text-white">
--- components/Who/Card.js
+++ components/Who/Card.js
@@ -1,11 +1,11 @@
-import React from "react";
-
+import styles from "../../styles/Card.module.css";
 export const Card = ({ mission }) => {
   return (
     <div className="bg-white flex flex-1 flex-grow flex-col justify-start items-center p-4 py-6 m-2 shadow-xl rounded-md">
-      <div className="h-20 my-3">
-        <img src={mission.src} alt="" className="h-full"/>
-      </div>
+      <div
+        className={"my-3 rounded-full " + styles.cardBg}
+        style={{ backgroundImage: "url(" + mission.src + ")" }}
+      ></div>
       <h1 className="text-2xl text-blue-900 my-3">{mission.title}</h1>
       <p className="text-sm text-blue-900">{mission.text}</p>
     </div>
